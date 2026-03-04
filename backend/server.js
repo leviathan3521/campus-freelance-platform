6335b9f45cadc0dc5b48250be9b071e3c65e5a0a
NISHANTH KUMAR R <2024503521@student.annauniv.edu>
2026-03-04 13:50:58 +0530

    Refactor server.js to use dynamic port and clean code

--- backend/server.js
+++ backend/server.js
@@ -9,17 +9,17 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
- 
+
 app.use("/api/auth", require("./routes/auth"));
-
 app.use("/api/tasks", require("./routes/task"));
-
 app.use("/api/applications", require("./routes/application"));
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
